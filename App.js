4319f02207e430efb0c2c621d8df7381c0b05513
Saeed Abdullah <me@saeedabdullah.com>
2021-01-29 18:36:49 -0500

    Basic functionalities

--- App.js
+++ App.js
@@ -5,11 +5,42 @@ import { Images, Colors, Metrics } from './App/Themes';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image style={{ height: Metrics.images.large, width: Metrics.images.large }}
-        source={Images.student1} />
+      <View style={styles.card}>
+
+        <View style={styles.pictureView}>
+          <Image style={styles.picture}
+            source={Images.student1} />
+
+          <View style={styles.pictureDetails}>
+            <Text style={{ fontWeight: 'bold' }}>Student Name</Text>
+            <Text style={{ fontWeight: '500' }}>ID: 00000000</Text>
+          </View>
+        </View>
+
+        <View style={styles.profileRowItem}>
+          <Text style={{ fontWeight: 'bold' }}>Year</Text>
+          <Text style={{ fontWeight: 'bold' }}>Major</Text>
+          <Text style={{ fontWeight: 'bold' }}>Minor</Text>
+        </View>
+        <View style={[styles.profileRowItem, { marginTop: 0, marginRight: 10 }]}>
+          <Text>Junior</Text>
+          <Text style={{ paddingRight: 25 }}>HCDD</Text>
+          <Text style>AI</Text>
+        </View>
+
+        <View style={styles.profileRowItem}>
+          <Text style={{ fontWeight: 'bold' }}>Campus</Text>
+          <Text style={{ fontWeight: 'bold' }}>Email</Text>
+        </View>
+        <View style={[styles.profileRowItem, { marginTop: 0 }]}>
+          <Text style={{ paddingLeft: 10 }}>University Park</Text>
+          <Text>sxn000@psu.edu</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +49,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    padding: Metrics.doubleBaseMargin,
+    width: Metrics.screenWidth * .9,
+    borderWidth: Metrics.borderWidth,
+    borderRadius: Metrics.buttonRadius
+  },
+  pictureView: {
+    marginLeft: Metrics.marginHorizontal,
+    marginRight: Metrics.marginHorizontal,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  picture: {
+    height: Metrics.images.large,
+    width: Metrics.images.large,
+    borderRadius: Metrics.images.large * 0.5
+  },
+  pictureDetails: {
+    flexDirection: 'column',
+    marginLeft: Metrics.marginHorizontal,
+    marginRight: Metrics.marginHorizontal,
+  },
+  profileRowItem: {
+    marginTop: Metrics.marginVertical,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
 });
