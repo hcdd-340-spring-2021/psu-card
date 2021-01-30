e820333a46e43fa95c99427769d0503ebd8963f1
Saeed Abdullah <me@saeedabdullah.com>
2021-01-30 16:38:56 -0500

    Starting template

--- App.js
+++ App.js
@@ -5,11 +5,42 @@ import { Images, Colors, Metrics } from './App/Themes';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image style={{ height: Metrics.images.large, width: Metrics.images.large }}
-        source={Images.student4} />
+      <View style={styles.card}>
+
+        <View style={styles.pictureView}>
+          <Image style={styles.picture}
+            source={Images.student4} />
+
+          <View style={styles.pictureDetails}>
+            <Text>Student Name</Text>
+            <Text>ID: 00000000</Text>
+          </View>
+        </View>
+
+        <View style={styles.profileRowItem}>
+          <Text>Year</Text>
+          <Text>Major</Text>
+          <Text>Minor</Text>
+        </View>
+        <View style={styles.profileRowItem}>
+          <Text>Junior</Text>
+          <Text>HCDD</Text>
+          <Text style>AI</Text>
+        </View>
+
+        <View style={styles.profileRowItem}>
+          <Text>Campus</Text>
+          <Text>Email</Text>
+        </View>
+        <View style={styles.profileRowItem}>
+          <Text>University Park</Text>
+          <Text>sxn000@psu.edu</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +49,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    padding: Metrics.doubleBaseMargin,
+    width: Metrics.screenWidth * .9,
+    borderWidth: Metrics.borderWidth,
+  },
+  pictureView: {
+    marginLeft: Metrics.marginHorizontal,
+    marginRight: Metrics.marginHorizontal,
+
+  },
+  picture: {
+    height: Metrics.images.large,
+    width: Metrics.images.large,
+    borderWidth: 1,
+    borderColor: Colors.psudark,
+  },
+  pictureDetails: {
+    marginLeft: Metrics.marginHorizontal,
+    marginRight: Metrics.marginHorizontal,
+  },
+  profileRowItem: {
+    marginTop: Metrics.marginVertical,
+  }
 });
