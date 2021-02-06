216fe73def0b910f839cb775d9c133ba9982a221
Saeed Abdullah <me@saeedabdullah.com>
2021-02-06 16:40:06 -0500

    Starter files for function component class activity

--- App.js
+++ App.js
@@ -4,19 +4,10 @@ import { Colors, Images } from './App/Themes';
 import ProfileView from './App/Components/ProfileView'
 
 export default function App() {
-  const profile = {
-    image: Images.student4,
-    year: 'Junior',
-    name: 'Function Component',
-    id: '00000000',
-    major: 'HCDD',
-    minor: 'Psychology',
-    campus: 'University Park',
-    email: 'fxc000@psu.edu'
-  }
   return (
     <View style={styles.container}>
-      {ProfileView(profile)}
+      <Text> Use the function component to show the </Text>
+      <Text> profile card with your information </Text>
     </View>
   );
 }
